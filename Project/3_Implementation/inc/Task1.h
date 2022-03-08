adc5e009fdf03126e814a39c7c8ef1e1d033782c
Chaitali Rokhade <42509899+chaitalirokhade@users.noreply.github.com>
2022-03-09 02:29:56 +0530

    Add files via upload

---
+++ Project/3_Implementation/inc/Task1.h
@@ -0,0 +1,16 @@
+
+#ifndef TASK1_H_INCLUDED
+#define TASK1_H_INCLUDED
+
+#include <avr/io.h>
+#include<util/delay.h>
+
+#define seat_sense !(PIND &(1<<PD3))
+#define heater_set (PIND &(1<<PD4))
+#define heater_clear PORTD&=~(1<<PD4)
+#define led_set PORTD|=(1<<PD2)
+#define led_clear PORTD &=~(1<<PD2)
+
+void seat_sensing();
+
+#endif // TASK1_H_INCLUDED
